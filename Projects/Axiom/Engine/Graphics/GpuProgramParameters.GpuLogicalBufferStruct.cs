f8caede9e0c7c945ccc64ebdafa12946d450d103
Wolfgang Steffens <WolfgangSteff@gmail.com>
2011-06-13 11:26:06 +0200

    moved and finalized GpuLogicalBufferStruct to 2790

--- Projects/Axiom/Engine/Graphics/GpuProgramParameters.GpuLogicalBufferStruct.cs
+++ Projects/Axiom/Engine/Graphics/GpuProgramParameters.GpuLogicalBufferStruct.cs
@@ -7,11 +7,14 @@ namespace Axiom.Graphics
 {
     partial class GpuProgramParameters
     {
-        
-
+        /// <summary>
+        /// Container struct to allow params to safely & update shared list of logical buffer assignments
+        /// </summary>
         [OgreVersion(1, 7, 2790)]
         public class GpuLogicalBufferStruct
         {
+            #region Mutex
+
             [OgreVersion(1, 7, 2790)]
             public object Mutex 
             { 
@@ -21,15 +24,25 @@ namespace Axiom.Graphics
                 }
             }
 
+            #endregion
+
+            #region Map
+
             /// <summary>
             /// Map from logical index to physical buffer location
             /// </summary>
             [OgreVersion(1, 7, 2790)]
             public readonly GpuLogicalIndexUseMap Map = new GpuLogicalIndexUseMap();
 
+            #endregion
+
+            #region BufferSize
+
             /// Shortcut to know the buffer size needs
             [OgreVersion(1, 7, 2790)]
             public int BufferSize;
+
+            #endregion
         };
     }
 }
