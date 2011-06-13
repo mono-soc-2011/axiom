bb262482cb4a02e47e5815946f5c9ed1075fb08e
Wolfgang Steffens <WolfgangSteff@gmail.com>
2011-06-13 10:28:29 +0200

    moved GpuNamedConstantSerializer.cs to new file added GpuNamedConstant.cs (missed in last commit) fixed refactoring based compile errors

---
+++ Projects/Axiom/Engine/Graphics/GpuProgramParameters.GpuNamedConstantSerializer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Axiom.Serialization;
+
+namespace Axiom.Graphics
+{
+	public partial class GpuProgramParameters
+	{
+        /// <summary>
+        /// Simple class for loading / saving GpuNamedConstants
+        /// </summary>
+        public class GpuNamedConstantsSerializer : Serializer
+        {
+            public void ExportNamedConstants(GpuNamedConstants pConsts, string filename)
+            {
+#warning implement Endian.Native.
+                ExportNamedConstants(pConsts, filename, Endian.Little);
+            }
+
+            public void ExportNamedConstants(GpuNamedConstants pConsts, string filename, Endian endianMode)
+            {
+                throw new NotImplementedException();
+            }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="stream"></param>
+            /// <param name="pDest"></param>
+            public void ImportNamedConstants(Stream stream, GpuNamedConstants pDest)
+            {
+                throw new NotImplementedException();
+            }
+        }
+	}
+}
